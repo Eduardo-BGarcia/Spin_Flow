0db326575e61dba6f4697c08004d6c03d14cddae
Eduardo-BGarcia <eduardobgarciax134abc39gmail.com>
2025-06-05 21:56:33 -0300

    [ FIX ] Correção de nomeclaturas e adição de estruturas faltantes.

---
+++ lib/dashboard/abas/recados_abas.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RecadosAbas extends StatelessWidget {
+  const RecadosAbas({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // Apenas um placeholder por enquanto
+    return const Center(
+      child: Text('Conteúdo da Aba Recados'),
+    );
+  }
+}
